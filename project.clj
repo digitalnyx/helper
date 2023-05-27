92abc56bb33f1cf7f5d493102509d06dfde8d7b5
Brandon White <brandon.ray.white@gmail.com>
2023-05-27 14:19:07 -0600

    license fix

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   
   :url "http://example.com/FIXME"
   
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url ""}
   
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
